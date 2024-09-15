585170853ce471e514a365a1ee8d3524132c400d
Akib558 <xenonsia1234@gmail.com>
2024-09-15 18:29:06 +0600

    resolved the not recognizing subfolders issue

---
+++ avim_local/lazy/conform.nvim/lua/conform/formatters/deno_fmt.lua
@@ -0,0 +1,26 @@
+local extensions = {
+  javascript = "js",
+  javascriptreact = "jsx",
+  json = "json",
+  jsonc = "jsonc",
+  markdown = "md",
+  esmodule = "mjs",
+  typescript = "ts",
+  typescriptreact = "tsx",
+}
+---@type conform.FileFormatterConfig
+return {
+  meta = {
+    url = "https://deno.land/manual/tools/formatter",
+    description = "Use [Deno](https://deno.land/) to format TypeScript, JavaScript/JSON and markdown.",
+  },
+  command = "deno",
+  args = function(self, ctx)
+    return {
+      "fmt",
+      "-",
+      "--ext",
+      extensions[vim.bo[ctx.buf].filetype],
+    }
+  end,
+}
